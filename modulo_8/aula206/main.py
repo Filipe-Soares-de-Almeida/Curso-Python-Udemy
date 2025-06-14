53a0a8c2918cc77ae650f8d78c43a3eed31cb416
filipe-ig <filipe.soares@redeig.com>
2025-06-14 10:59:28 -0400

    progresso diario

--- modulo_8/aula206/main.py
+++ modulo_8/aula206/main.py
@@ -2,16 +2,28 @@
 # Doc: https://pymysql.readthedocs.io/en/latest/
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: https://github.com/PyMySQL/PyMySQL
+import os
+
+import dotenv
 import pymysql
 
+dotenv.load_dotenv()
+
 connection = pymysql.connect(
-    host='localhost',
-    user='usuario',
-    password='senha',
-    database='base_de_dados',
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE'],
 )
 
 with connection:
     with connection.cursor() as cursor:
-        # SQL
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS customers ('
+            'id INT NOT NULL AUTO_INCREMENT, '
+            'nome VARCHAR(50) NOT NULL, '
+            'idade INT NOT NULL, '
+            'PRIMARY KEY (id)'
+            ') '
+        )
         print(cursor)
