ee61d1c45ac12ba4df36fe5d3371a4a916ec06ee
filipe-ig <filipe.soares@redeig.com>
2025-06-25 17:02:48 -0400

    progresso diario

--- modulo_9/django_basico/blog/views.py
+++ modulo_9/django_basico/blog/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 def blog(request):
     print('blog')
-    return HttpResponse('blog do app')
+    return render(request, 'blog/index.html')
 
 def exemplo(request):
     print('exemplo')
-    return HttpResponse('exemplo do app')
+    return render(request, 'blog/exemplo.html')
