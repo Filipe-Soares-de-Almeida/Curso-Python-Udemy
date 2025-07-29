d2df6117c77b6d1cd40763e544e39890e982eedb
filipe-ig <filipe.soares@redeig.com>
2025-07-29 09:18:28 -0400

    Progresso diario

--- modulo_10/django_agenda/contact/models.py
+++ modulo_10/django_agenda/contact/models.py
@@ -1,5 +1,7 @@
+from tabnanny import verbose
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,7 +10,12 @@ from django.utils import timezone
 # email (email), created_date (date), description (text)
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (image)
+
 class Category(models.Model):
+  class Meta:
+    verbose_name = 'Category'
+    verbose_name_plural = 'Categories'
+
   name = models.CharField(max_length=50)
 
   def __str__(self):
@@ -24,8 +31,7 @@ class Contact(models.Model):
   show = models.BooleanField(default=True)
   picture = models.ImageField(upload_to='pictures/%Y/%m/%d', blank=True, null=True)
   category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
-
-  # owner = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True)
+  owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
   def __str__(self):
     return f"{self.first_name} {self.last_name}"
